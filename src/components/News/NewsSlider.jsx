613f0d88d06228f27ac774cd0e307e911710d611
delower-hasan <dhhasansaha11@gmail.com>
2024-05-02 17:53:52 +0600

    dynamic route use and filter option hide

--- src/components/News/NewsSlider.jsx
+++ src/components/News/NewsSlider.jsx
@@ -1,85 +1,84 @@
-
-
-import React, { useEffect, useState } from 'react';
-import 'react-multi-carousel/lib/styles.css';
+import React, { useEffect, useState } from "react";
+import "react-multi-carousel/lib/styles.css";
 import Carousel from "react-multi-carousel";
-import RightButton from '../../assets/RightButton1.png';
-import n1 from '../../assets/lnews.png'
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import RightButton from "../../assets/RightButton1.png";
+import n1 from "../../assets/lnews.png";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
+const NewsSlider = ({ type }) => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-const NewsSlider = ({type}) => {
+  const [news, setNews] = useState([]);
 
-
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_LIVE_API_URL}/api/v1/blogs/all`
+        );
+        const allBlogs = response.data.data.blogs;
+        const articles = allBlogs.filter((blog) => blog.type === type);
+        setNews(articles);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
     };
+    fetchUsers();
+  }, [type]);
 
-
-    const [news, setNews] = useState([]);
-
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await axios.get('http://localhost:8000/api/v1/blogs/all');
-                const allBlogs = response.data.data.blogs;
-                const articles = allBlogs.filter(blog => blog.type === type);
-                setNews(articles);
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-        fetchUsers();
-    }, [type]);
-
-    
-
-    return (
-        <div className='news_slider_wrapper gap-5' style={{ padding: "80px 80px" }}>
-            <Carousel responsive={responsive}>
-
-                {news.map((item, index) => (
-                    <div key={index} className='col-lg-4 col-12 w-100'>
-                        <div className='text-start bg-transparent'>
-                            <img className='w-100' src={
-                                item?.image
-                                    ? `${process.env.NODE_ENV !== "production"
-                                        ? import.meta.env.VITE_LOCAL_API_URL
-                                        : import.meta.env.VITE_LIVE_API_URL
-                                    }/api/v1/uploads/${item?.image}`
-                                    : n1
-                            } alt={item.title} />
-                            <div className='details'>
-                                <p>{item.title}</p>
-                                <div className='view_morebtn1'>
-                                    <Link to={`/newsDetails/${item._id}`}>
-                                        <button>View More <img  src={RightButton} alt="RightButton" /> </button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-
-            </Carousel>
-        </div>
-    );
+  return (
+    <div className="news_slider_wrapper gap-5" style={{ padding: "80px 80px" }}>
+      <Carousel responsive={responsive}>
+        {news.map((item, index) => (
+          <div key={index} className="col-lg-4 col-12 w-100">
+            <div className="text-start bg-transparent">
+              <img
+                className="w-100"
+                src={
+                  item?.image
+                    ? `${
+                        process.env.NODE_ENV !== "production"
+                          ? import.meta.env.VITE_LOCAL_API_URL
+                          : import.meta.env.VITE_LIVE_API_URL
+                      }/api/v1/uploads/${item?.image}`
+                    : n1
+                }
+                alt={item.title}
+              />
+              <div className="details">
+                <p>{item.title}</p>
+                <div className="view_morebtn1">
+                  <Link to={`/newsDetails/${item._id}`}>
+                    <button>
+                      View More <img src={RightButton} alt="RightButton" />{" "}
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default NewsSlider;
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,44 +1,46 @@
 import NewsCard from "../../components/News/NewsCard";
-import NewsHeader from "../../components/News/NewsHeader"
-import './News.css';
+import NewsHeader from "../../components/News/NewsHeader";
+import "./News.css";
 import Pagination from "../../components/Pagination/Pagination";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
 const News = () => {
-    const [news, setNews] = useState([]);
+  const [news, setNews] = useState([]);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await axios.get('http://localhost:8000/api/v1/blogs/all');
-                // setNews(response.data.data.blogs)
-                const allBlogs = response.data.data.blogs;
-                const articles = allBlogs.filter(blog => blog.type === 'news');
-                setNews(articles);
-                console.log(response.data.data.blogs, "news data get thgeheirleijrlskflksdfhsdhf")
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-        fetchUsers();
-    }, []);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_LIVE_API_URL}/api/v1/blogs/all`
+        );
+        // setNews(response.data.data.blogs)
+        const allBlogs = response.data.data.blogs;
+        const articles = allBlogs.filter((blog) => blog.type === "news");
+        setNews(articles);
+        console.log(
+          response.data.data.blogs,
+          "news data get thgeheirleijrlskflksdfhsdhf"
+        );
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchUsers();
+  }, []);
 
+  return (
+    <div className="container">
+      <NewsHeader />
+      {news.map((data) => (
+        <>
+          <NewsCard data={data} />
+        </>
+      ))}
 
-    return (
-        <div className="container">
-            <NewsHeader />
-            {news.map((data) => (
-                <>
-                    <NewsCard data={data} />
-                </>
-            ))}
+      <Pagination />
+    </div>
+  );
+};
 
-            <Pagination />
-
-        </div>
-    )
-}
-
-export default News
+export default News;
