ac4d19e690e4879bf4d5347c22a96e1bb2012a68
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2024-05-25 13:16:06 +0600

    ::update

--- src/components/Dashboard/Messages/Messages.jsx
+++ src/components/Dashboard/Messages/Messages.jsx
@@ -5,7 +5,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import chatAvatar from "../../../assets/avatar.png";
 import messageProfile from "../../../assets/coach_img.png";
 import mDetails from "../../../assets/m_details.svg";
-import send from "../../../assets/send.png";
+import send from "../../../assets/send-icon.svg";
 import { useGetUserByIdQuery } from "../../../features/auth/authApi";
 import {
   chatApi,
@@ -162,7 +162,7 @@ const Messages = () => {
                       onChange={(e) => setSearchText(e.target.value)}
                     />
                   </div> */}
-                 
+
                   <ChatProfiles user={user} searchText={searchText} />
                 </div>
               </div>
@@ -285,20 +285,29 @@ const Messages = () => {
                   </div>
 
                   <div className="upload_img">
-                    <label htmlFor="upload">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none">
-                        <path
-                          d="M16.5 6V17.5C16.5 19.71 14.71 21.5 12.5 21.5C10.29 21.5 8.5 19.71 8.5 17.5V5C8.5 3.62 9.62 2.5 11 2.5C12.38 2.5 13.5 3.62 13.5 5V15.5C13.5 16.05 13.05 16.5 12.5 16.5C11.95 16.5 11.5 16.05 11.5 15.5V6H10V15.5C10 16.88 11.12 18 12.5 18C13.88 18 15 16.88 15 15.5V5C15 2.79 13.21 1 11 1C8.79 1 7 2.79 7 5V17.5C7 20.54 9.46 23 12.5 23C15.54 23 18 20.54 18 17.5V6H16.5Z"
-                          fill="#2B3674"
-                        />
-                      </svg>
-                    </label>
-                    <input id="upload" type="file" className="bg-none d-none" />
+                    <div>
+                      <label htmlFor="upload">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none">
+                          <path
+                            d="M16.5 6V17.5C16.5 19.71 14.71 21.5 12.5 21.5C10.29 21.5 8.5 19.71 8.5 17.5V5C8.5 3.62 9.62 2.5 11 2.5C12.38 2.5 13.5 3.62 13.5 5V15.5C13.5 16.05 13.05 16.5 12.5 16.5C11.95 16.5 11.5 16.05 11.5 15.5V6H10V15.5C10 16.88 11.12 18 12.5 18C13.88 18 15 16.88 15 15.5V5C15 2.79 13.21 1 11 1C8.79 1 7 2.79 7 5V17.5C7 20.54 9.46 23 12.5 23C15.54 23 18 20.54 18 17.5V6H16.5Z"
+                            fill="#2B3674"
+                          />
+                        </svg>
+                      </label>
+                    </div>
+
+                    <div>
+                      <input
+                        id="upload"
+                        type="file"
+                        className="bg-none d-none"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
